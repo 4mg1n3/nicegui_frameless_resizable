cc7d7433046a65634f2045c7e8e2d9c085d36130
4mg1n3 <64701314+4mg1n3@users.noreply.github.com>
2024-05-08 00:01:21 +0200

    a

---
+++ README.md
@@ -0,0 +1,15 @@
+# Some info
+
+### What is this ?
+This project is just a quick work on the reimplementation of mouse resizability and movability for a NiceGUI frameless window on Windows.
+
+### A small disclaimer :
+This code is probably not very efficient and can be a bit ressource hungry. It's good enough for my needs but probably not for production.
+
+### What can be improved :
+Currently the east and south resize are still using the same function and therefore one of the two border snaps to the mouse.
+There is also no resize function for the top and the left border but they are easily implementable with the code given.
+
+---
+
+I hope this small repo can help people stuck on this, don't hesitate to star it for visibility ^_^
---
+++ resizable_frameless.py
@@ -0,0 +1,80 @@
+from nicegui import ui, app
+import win32api
+import time
+import threading
+
+
+
+#----------------------------------------------------
+#    Custom drag zone to move and resize the window |
+#----------------------------------------------------
+
+def create_custom_div(style, func):
+    div = ui.html('<div style="{}"></div>'.format(style))
+    
+    div.on('mousedown', func)
+
+    return div
+
+#----------------------------------------------------
+# We resize and move the window in a separate thread |
+#----------------------------------------------------
+async def on_drag_start_resize(event):
+    initial_window_pos = await app.native.main_window.get_position()
+    threading.Thread(target=resize_window, args=(event, initial_window_pos)).start()
+
+def resize_window(event, initial_window_pos):
+
+    # while the left mouse button is pressed
+    while win32api.GetAsyncKeyState(0x01) < 0:
+        cursor_x, cursor_y = win32api.GetCursorPos()
+
+        new_width = cursor_x - initial_window_pos[0]
+        new_height = cursor_y - initial_window_pos[1]
+
+        app.native.main_window.resize(new_width, new_height)
+        time.sleep(0.01)
+
+async def on_drag_start_move(event):
+    initial_window_pos = await app.native.main_window.get_position()
+    initial_mouse_pos = win32api.GetCursorPos()
+    threading.Thread(target=move_window, args=(event, initial_window_pos, initial_mouse_pos)).start()
+
+def move_window(event, initial_window_pos, initial_mouse_pos):
+    while win32api.GetAsyncKeyState(0x01) < 0:
+        cursor_x, cursor_y = win32api.GetCursorPos()
+
+        new_x = cursor_x - initial_mouse_pos[0]
+        new_y = cursor_y - initial_mouse_pos[1]
+
+        app.native.main_window.move(initial_window_pos[0] + new_x, initial_window_pos[1] + new_y)
+        time.sleep(0.01)
+
+#----------------------------------------------------
+#    Styling                                         |
+#----------------------------------------------------
+
+# We create the different drag zones
+create_custom_div('position: absolute; bottom: 0; left: 0; width: 100%; height: 10px; cursor: s-resize; border-bottom: 4px solid #151515;', on_drag_start_resize)
+create_custom_div('position: absolute; top: 0; right: 0; width: 10px; height: 100%; cursor: e-resize; border-right: 4px solid #151515;', on_drag_start_resize)
+create_custom_div('position: absolute; bottom: 0; right: 0; width: 10px; height: 10px; cursor: se-resize;', on_drag_start_resize)
+
+
+with ui.header().classes(replace='row items-center bg-dark').style('padding-right: 10px;') as header:
+    with ui.tabs() as tabs:
+        ui.tab('A')
+        ui.tab('B')
+        ui.tab('C')
+    ui.space()
+    ui.button(on_click=lambda: app.shutdown(), icon='close').props('flat color=red')
+
+    # We assign the move event to the header
+    header.on('mousedown', on_drag_start_move)
+
+    
+
+
+app.native.window_args['easy_drag'] = False
+
+ui.run(title='Resizable Frameless Window', native=True, frameless=True, window_size=(800, 600), reload=False)
+
